c80204b7389835a2e7634802838b334be4dab1f3
Evgeny Pankov <volvovnl123@gmail.com>
2023-07-18 09:40:36 +0300

    CW #30 TreeSet, HashMap, LinkedHashMap

---
+++ src/telran/util/test/TreeSetTests.java
@@ -0,0 +1,35 @@
+package telran.util.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TreeSetTests {
+	Integer[] numbers = { 10, -30, 13, 48, 22, 17, 23 };
+	TreeSet<Integer> treeSet;
+
+	@BeforeEach
+	void setUp() {
+		treeSet = new TreeSet<>();
+		Arrays.stream(numbers).forEach(treeSet::add);
+	}
+
+	@Test
+	void subTreeTest() {
+		NavigableSet<Integer> subSet = treeSet.subSet(-30, true, 13, true);
+		Integer[] expected1 = { -30, 10, 13 };
+		assertArrayEquals(expected1, subSet.toArray(Integer[]::new));
+		Integer[] expected2 = { -30, 10, 13, 17, 22, 23, 48 };
+		assertArrayEquals(expected2, treeSet.toArray(Integer[]::new));
+		Integer[] expected3 = { 10, 13, 17, 22, 23, 48 };
+		subSet.remove(-30);
+		assertArrayEquals(expected3, treeSet.toArray(Integer[]::new));
+		treeSet.remove(13);
+		Integer[] expected4 = { 10 };
+		assertArrayEquals(expected4, subSet.toArray(Integer[]::new));
+		treeSet.add(13);
+	}
+}
